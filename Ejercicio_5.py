96661ef5ecd77adfa46ea5fc943099a2d7b49228
CarlosMarcelo2 <130719547+CarlosMarcelo2@users.noreply.github.com>
2023-05-05 23:54:40 -0400

    Add files via upload

---
+++ Ejercicio_5.py
@@ -0,0 +1,9 @@
+cantidad = int(input("Ingresa Cantidad a Invertir: "))
+
+interes = float(input("Ingresa el Interés Anual: "))
+
+años = int(input("Ingresa el Número de Años: "))
+
+capital = int(cantidad*interes*años)           
+
+print(f'El Capital obtenido es de: $ {capital}')
