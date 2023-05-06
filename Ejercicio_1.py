dd6e15350729b65fe605bebc9dc3b3bfe5f207b8
CarlosMarcelo2 <130719547+CarlosMarcelo2@users.noreply.github.com>
2023-05-05 22:02:19 -0400

    Ejercicio1

---
+++ Ejercicio_1.py
@@ -0,0 +1,4 @@
+Numero_1 = int(input("Ingresa el primer número: "))
+Numero_2 = int(input("Ingresa el segundo número: "))
+Sumar = Numero_1 + Numero_2
+print("La suma de ambos números es = ", Sumar)
