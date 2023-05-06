9a02825c549a69622fda6fe2f4c1117ebcf60ed6
CarlosMarcelo2 <130719547+CarlosMarcelo2@users.noreply.github.com>
2023-05-06 11:50:14 -0400

    Add files via upload

---
+++ Ejercicio_6.py
@@ -0,0 +1,7 @@
+Peso_payaso = 112
+Peso_muñeca = 75
+num_payaso = int(input("Ingrese número de Payaso(s):  "))
+num_muñeca = int(input("Ingrese número de Muñeca(s):  "))
+Peso_total = num_payaso * Peso_payaso + num_muñeca * Peso_muñeca
+print(f'El peso total del paquete es: {Peso_total}')
+
