2b4efb0fa17cbf6ea35891d58838d3ac7681ae5e
CarlosMarcelo2 <130719547+CarlosMarcelo2@users.noreply.github.com>
2023-05-08 18:44:28 -0400

    Add files via upload

---
+++ Ejercicio_11.py
@@ -0,0 +1,9 @@
+Edad_Minina = 16
+Ingresos = 1000
+años = int(input("Ingrese su edad :"))
+ingreso_mensual = int(input("Ingrese Sueldo Mensual :"))
+if (años > Edad_Minina and ingreso_mensual >= Ingresos):
+    print("Ud. debe Tributar")
+else:
+    print("Ud. no debe Tributar")
+
